ef7612d52935584a98fac5388182688d77998920
amar-music <amar.music@live.nl>
2023-02-17 20:46:26 +0100

    Create scrape.py

---
+++ GAN Detection/GANimageDetection-main/scrape.py
@@ -0,0 +1,17 @@
+import csv
+import os
+import requests
+
+path = os.getcwd()
+TYPE = "real"   # "real" or "fake"
+
+with open("image_sources/df_" + TYPE + ".csv") as csvfile:
+    csvrows = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in csvrows:
+        filename = ("images_" + TYPE + "/" + row[0] + ".png")
+        url = row[1].replace(" ", "+")
+        print(url)
+        result = requests.get(url, stream=True)
+        if result.status_code == 200:
+            image = result.raw.read()
+            open(filename, "wb").write(image)
